f2abcb98dd0704f0eb92cbf67c5df4809d54b1a3
Trung Huynh <trunghlt@gmail.com>
2011-03-17 22:30:02 +0000

    Added Amazon DVD review scraper and ThreadDistributor module

--- src/Scraper/ThreadDistributor.py
+++ src/Scraper/ThreadDistributor.py
@@ -1,20 +1,37 @@
+""" Thread Distributor
+
+Ver     : 0.1
+Author  : Trung Huynh
+
+"""
 from Queue import Queue
 from threading import Thread
+from semetric.util.logging import CommentTimerLog
+
 
 class Worker(Thread):
     
     def __init__(self, distributor, name=None):
-        self.name = name
+        Thread.__init__(self)
         self.queue = Queue()
         self.distributor = distributor
-        Thread.__init__(self)
+        if name is not None: self.name = name
         
     def run(self):
         for task in iter(self.distributor.inp_queue.get, "STOP"):
             task.worker = self
+            self.distributor.working += 1
             for result in task.run():
-                self.distributor.out_queue.put(result)
-                
+                if result is not None:
+                    self.distributor.out_queue.put(result)
+            self.distributor.working -= 1
+            
+            # Stop if no worker is working and queue is empty. It is 
+            # reasonable because no new task can be generated at this point.
+            if (self.distributor.working == 0) \
+            and (self.distributor.inp_queue.empty()):
+                self.distributor.stop()
+            
     def send(self, msg, to):
        self.distributor.send(msg, to) 
     
@@ -32,12 +49,23 @@ class Task:
 
 class ThreadDistributor:
 
-    def __init__(self):
+
+    class StopTask(Task):
+        
+        def run(self):
+            self.distributor.out_queue.put("STOP")
+            yield None
+
+
+    def __init__(self, n_workers=0):
         self.inp_queue = Queue()
         self.out_queue = Queue()
-        self.workers = []
         self.name_id_map = {}
-        self.n_workers = 0
+        self.n_workers = n_workers
+        self.workers = []
+        self.working = 0
+        for i in xrange(n_workers):
+            self.add_worker()
         
     def send(self, msg, to):
         if type(to) is int:
@@ -48,22 +76,28 @@ class ThreadDistributor:
     def add_worker(self, name=None):
         if name is not None: self.name_id_map[name] = self.n_workers
         self.workers.append(Worker(self, name))
+        self.n_workers += 1
         
     def run(self):
         for t in self.workers: t.start()
         for t in self.workers: t.join()
         
-    def add_task(self, data, task):
+    def add_task(self, task, data=None):
         task.distributor = self
         task.inp = data
         self.inp_queue.put(task)
         
     def stop(self):
+        self.add_task(ThreadDistributor.StopTask())
         for i in xrange(self.n_workers):
             self.inp_queue.put("STOP")
+            
+    def results(self):
+        for item in iter(self.out_queue.get, "STOP"):
+            yield item
         
     
-class SquareTask(Task):
+class ComputationTask(Task):
     
     def run(self):
         yield self.inp**2;
@@ -72,14 +106,15 @@ class SquareTask(Task):
 class InitializeTask(Task):
     
     def run(self):
-        for i in xrange(5):     
-            self.distributor.add_task(i, SquareTask())
+        for i in xrange(1000000):     
+            self.distributor.add_task(ComputationTask(), i)
         self.distributor.stop()
+        yield None
+        
         
 if __name__ == "__main__":
-    distributor = ThreadDistributor()
-    for i in xrange(10): distributor.add_worker()
-    distributor.add_task(None, InitializeTask())
+    distributor = ThreadDistributor(20)
+    distributor.add_task(InitializeTask())
+    timer = CommentTimerLog("ThreadDistributor", incremental_print=True) 
+    timer.append("Marathon")
     distributor.run()
-    for v in distributor.out_queue.get():
-        print v
--- src/Scraper/amazon_dvd.py
+++ src/Scraper/amazon_dvd.py
@@ -1,3 +1,5 @@
+import os
+import csv
 import logging
 import re
 import urllib2
@@ -6,56 +8,94 @@ import lxml.html
 import time
 from dateutil.parser import parse
 from semetric.util.scraper import ScraperLogMessage, Scraper, ScraperDownloadError
+from ThreadDistributor import ThreadDistributor, Task
 
-class AmazonFutureReleaseScraper():
-        
-    #URL="http://www.amazon.co.uk/mn/search/ajax/ref=sr_pg_3_ajx_0?rh=n:229816,p_69:30x-7y,p_n_date:182239031&page=%(page)d&bbn=229816&sort=-releasedate&ie=UTF8&qid=1296143906&tab=music&pageTypeID=229816&fromHash=undefined&fromRH=n:229816,p_69:30x-7y&section=ATF,BTF&version=2&rrid=16HJ0F353VJJ0NQKNFYD"
-    URL="http://www.amazon.co.uk/mn/search/ajax/ref=sr_pg_2?rh=n%%3A283926&page=%(page)d&sort=reviewrank&ie=UTF8&qid=1300376652&tab=dvd&pageTypeID=283926&fromHash=undefined&fromRH=n%%3A283926&section=ATF,BTF&fromApp=gp%%2Fsearch&fromPage=results&version=2&rrid=1YF9C2Y1ZRP1E82X11MH"
-
-    def __init__(self):
-        self.page = 1
-        pass
-        
-    def get(self):
-        self.results = []
-        self.page = 1
-        while True:
-            for message in self.get_page(self.page): 
-                if message is None: 
-                    yield ScraperLogMessage("Completed !", logging.DEBUG)
-                    return
-                yield message
-            self.page += 1
-        
-    def get_page(self, page):
-        yield ScraperLogMessage("Downloading page %s" % page, logging.DEBUG)
-        url = self.URL % {"page": page}
+def download(url, max_retries=5):    
+    for i in xrange(max_retries):
         try:
-            jsonStr = "[" + urllib2.urlopen(url).read().replace("&&&", ",") + "]"
+            return urllib2.urlopen(url).read()
         except Exception, e:
-            print e
-        
-        """
-        try:
-            yield ScraperLogMessage("Downloading page %s" % page, logging.DEBUG)
-            url = self.URL % {"page": page}
-            jsonStr = u"[" + urllib2.urlopen(url).read().replace(u"&&&", u",") + u"]"
-        except:
-            yield None
-        """
-        try:
-            data = demjson.decode(jsonStr, encoding="latin-1")
-        except Exception, e:
-            print e
+            print "Retries: %d, url: %s" % (i + 1, url)
+            pass
+    return None
+    
 
-        """
-        try:
-            data = demjson.decode(jsonStr, encoding="latin-1")
-        except demjson.JSONDecodeError:
-            yield ScraperLogMessage("Parsing error!!!", logging.DEBUG)
-            yield None
-        """
+class Archive:
+    
+    def __init__(self, path):
+        self.path = path
+        
+    def save(self, content, filename):        
+        f = open(os.path.join(self.path, filename), "w")
+        f.write(content)
+        f.close()
+
+
+class ReviewScrapeTask(Task):
+
+    URL = "http://www.amazon.co.uk/Firefly-Complete-DVD-Region-NTSC/product-reviews/%(code)s/ref=sr_1_1_cm_cr_acr_txt?ie=UTF8&showViewpoints=1&pageNumber=%(page)d"
+    archive = Archive("Amazon/DVD/reviews")
+    output = csv.writer(open("Amazon/DVD/reviews.csv", "w"), delimiter="\t", quotechar="\"")
+    
+    def run(self):
+        code, page, name = self.inp["code"], self.inp["page"], self.inp["name"]
+        html = download(self.URL % {"code": code, "page": page})
+        if html is None: return
+        
+        ReviewScrapeTask.archive.save(html, "%s_%d.html" % (code, page))
+        dom = lxml.html.fromstring(html)
+        
+        try: productReviews = dom.get_element_by_id("productReviews")
+        except KeyError: return 
+
+        self.distributor.add_task(ReviewScrapeTask(), {
+            "name": name, \
+            "code": code, \
+            "page": page + 1 \
+        }) 
+
+        for div in productReviews.cssselect("td > div"):
+            try:
+                rate = int(div.cssselect(".swSprite")[0].get("title")[0])
+            except Exception, e:
+                rate = -1
+                
+            try:
+                title = div.cssselect("b")[0].text
+            except Exception, e:
+                title = ""
             
+            content = ""
+            for e in div:
+                if e.tag == "p": content += e.text or ""
+                content += e.tail or ""
+                
+            logging.info("REVIEW dvd: %s, rate: %d, title: %s, content: %s"\
+                         % (name, rate, title, content.strip()))
+                         
+            self.output.writerow([\
+                rate, \
+                name.encode("utf-8"), \
+                title.encode("utf-8"), \
+                content.strip().encode("utf-8")]\
+            )
+        
+        yield None       
+
+
+class SearchTask(Task):
+        
+    URL="http://www.amazon.co.uk/mn/search/ajax/ref=sr_pg_2?rh=n%%3A283926&page=%(page)d&sort=reviewrank&ie=UTF8&qid=1300376652&tab=dvd&pageTypeID=283926&fromHash=undefined&fromRH=n%%3A283926&section=ATF,BTF&fromApp=gp%%2Fsearch&fromPage=results&version=2&rrid=1YF9C2Y1ZRP1E82X11MH"
+    archive = Archive("Amazon/DVD/searchs")
+    
+    def run(self):
+        page = self.inp
+        content = download(self.URL % {"page": page})
+        if content is None: return
+        
+        SearchTask.archive.save(content, "%d.html" % page)
+        jsonStr = "[%s]" % content.replace("&&&", ",")
+        data = demjson.decode(jsonStr, encoding="latin-1")
         found = False
         for key in ["results-atf", "results-btf"]:
             for chunk in data:
@@ -64,34 +104,32 @@ class AmazonFutureReleaseScraper():
                     except: continue
                     
                     for e in doms.cssselect(".result"): 
-
                         found = True
                     
-                        try:    releaseId = e.get("name").split("_")[0]
-                        except: releaseId = "Unknown"
+                        try:    code = e.get("name").split("_")[0]
+                        except: code = None
                     
-                        try:    release = e.cssselect("a.title")[0].text
-                        except: release = "Unkown"
+                        try:    name = e.cssselect("a.title")[0].text
+                        except: name = None
                         
-                        try:    artist = e.cssselect(".ptBrand")[0].text_content()[3:]
-                        except: artist = "Unknown"
-                            
-                        try:
-                            fastTrack = e.cssselect(".fastTrack")[0].text
-                            m = re.search(r"[0-9]+\s[a-zA-Z]{,10}\s[0-9]+", fastTrack)
-                            date = int(time.mktime(parse(m.group(0)).timetuple()))
-                        except:
-                            date = "Unknown"
-                                
-                        yield ScraperLogMessage([artist, release, releaseId, date], logging.INFO)
-                        self.results.append([artist, release, releaseId, date])
+                        self.distributor.add_task(ReviewScrapeTask(), {\
+                            "name": name, \
+                            "code": code, \
+                            "page": 1 \
+                        }) 
+                        logging.info("SEARCH dvd: %s, code: %s, page: %d" \
+                                     % (name, code, page))
                         
-        if not found: yield None
-        
-        
+        if found: self.distributor.add_task(SearchTask(), page + 1)
+        yield None
+    
 if __name__ == "__main__":
-    logging.basicConfig(level = logging.DEBUG, format="%(asctime)s\t%(levelname)s\t%(message)s")
-    scraper = AmazonFutureReleaseScraper()
-    for message in scraper.get():
-        logging.log(message.level, message.message)
-    print scraper.results
+    logging.basicConfig(\
+        level = logging.DEBUG, \
+        filename="Amazon/DVD/log.txt", \
+        filemode="w", \
+        format="%(asctime)s\t%(levelname)s\t%(threadName)s\t%(message)s"\
+    )
+    distributor = ThreadDistributor(20)
+    distributor.add_task(SearchTask(), 1)
+    distributor.run()
