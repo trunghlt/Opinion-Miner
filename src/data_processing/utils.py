d796a28ff30193a5d5e6f72ae2d433f4fc0fa0d9
Trung Huynh <trung@vietnamwiki.net>
2011-03-30 20:05:39 +0100

    Added first data processing lib

---
+++ src/data_processing/utils.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#
+# Copyright 2011 Trung Huynh
+#
+# Licnesed under GNU GPL, Version 3.0; you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.gnu.org/licenses/gpl.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""A util module for text processing
+
+"""
+def erase(sub, string, char=" "):
+    """Find ``sub`` and replace them by a string including only ``char`` with 
+    same length as ``sub`` in ``string``.
+    
+    >>erase("Hello", "Hello world", "-")
+    >>---- world
+
+    """
+    return string.replace(sub, char*len(sub))
+    
+    
+class Amazon:
+
+    @staticmethod
+    def extract_movie_title(title):
+        """Extract real movie title from product
+        
+        """
